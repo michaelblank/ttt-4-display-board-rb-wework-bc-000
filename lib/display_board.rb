c4f658f49d8477dcbfbbc6b31288b16ac3dd4fb6
Michael Blank <michael.blank@wework.com>
2018-02-11 20:23:30 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-puts"Welcome to Tic Tac Toe!"
+puts "Welcome to Tic Tac Toe!"
 
 board = [" "," "," "," "," "," "," "," "," "]
 
